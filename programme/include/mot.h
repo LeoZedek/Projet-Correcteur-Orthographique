7803672265b6cbe0003676a66482a933e467acc3
Léo <leo.zedek@insa-rouen.fr>
2021-12-11 12:31:55 +0100

    Debut de l'implémentation Mot

---
+++ programme/include/mot.h
@@ -0,0 +1,25 @@
+/**
+ * \file mot.h
+ * \brief Implantation du TAD Mot
+ * \version 1.0
+ * \date 11/12/2021
+ *
+ */
+ 
+#ifndef __MOT__
+#define __MOT__
+
+/**
+ * \brief Le type Mot qui represente un mot
+ *
+ */
+
+typedef struct {
+	char *chaine; /**< la chaine de charactère du mot */
+	unsigned int longueur; /**< la longueru du mot */
+} Mot;
+
+
+
+#endif
+
