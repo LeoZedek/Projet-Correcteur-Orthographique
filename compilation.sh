706be9c22ea5e7f00736681172f4520c258baa04
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2021-12-13 23:57:08 +0100

    modification du script pour la création de fichier et la suppression de fichier

--- compilation.sh
+++ compilation.sh
@@ -6,6 +6,12 @@ make
 cd doc/latex/
 make
 cd ../../../
+
+if [ -d "./resultat" ];then
+rm -r resultat;
+fi
+
+mkdir resultat
 cp -f rapport/rapport.pdf ./resultat/rapport.pdf
 cp -f programme/bin/asispell ./resultat/asispell
 cp -f programme/doc/latex/refman.pdf ./resultat/documentation.pdf
