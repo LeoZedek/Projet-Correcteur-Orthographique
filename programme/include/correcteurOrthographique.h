f9dd41e38454d6043b8ea2ce43a132802e33fc65
Ruth Lioté <ruth.liote@insa-rouen.fr>
2021-12-19 01:49:18 +0100

    Ajout de la fonction CO_phraseEnMots dans correcteurorthographique.h et finition de la doc

--- programme/include/correcteurOrthographique.h
+++ programme/include/correcteurOrthographique.h
@@ -20,7 +20,7 @@ enum CO_ALPHABET = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 
 
 /**
- * \brief La constante CORRECTEURORTHOGRAPHIQUE_TailleMax définie la taille maximale du tableau de booléens.
+ * \brief La constante CORRECTEURORTHOGRAPHIQUE_TailleMax définie la taille maximale des tableaux utilisés.
  *
  */
  
@@ -34,14 +34,24 @@ const CO_TailleMax = 1000;
  
 typedef int CO_TableauBooleens[CO_TailleMax];
 
+/**
+ * \brief Le type structure CO_MotsDansPhrase permet de renvoyer un tableau de mots ainsi que leur position dans la phrase.
+ *
+ */
+ 
+typedef struct {
+	MOT_mot mot[CO_TailleMax];
+	int position[CO_TailleMax];
+} CO_MotsDansPhrase;
 
 /**
- * \fn void sontPresents(int tableauBooleens[], Mot mots[], Dictionnaire d)
+ * \fn CO_TableauBooleens sontPresents(Mot mots[], Dictionnaire dictionnaire)
  * \brief Fonction de vérification pour voir si une liste de mots est présente dans le dictionnaire ou non.
  *
  * \param tableauBooleens : un pointeur sur un entier
  * \param mots : un pointeur sur un mot
- * \param d : le dictionnaire
+ * \param dictionnaire : le dictionnaire
+ * \return CO_TableauBooleens, c'est-à-dire les booléens précisant si les mots sont présents ou non dans le dictionnaire.
  */
 
 CO_TableauBooleens CO_sontPresents(MOT_TableauDeMots mots[], DICTIONNAIRE_Dictionnaire dictionnaire); 
@@ -49,16 +59,24 @@ CO_TableauBooleens CO_sontPresents(MOT_TableauDeMots mots[], DICTIONNAIRE_Dictio
 
 
 /**
- * \fn void proposerMots(Mot mots[], Mot m, Dictionnaire d);
+ * \fn MOT_TableauDeMots proposerMots(Mot m, Dictionnaire dictionnaire)
  * \brief Fonction proposant une liste de mots corrects pouvant être la correction du mot donné en entrée.
  *
- * \param mots : un pointeur sur un mot
  * \param m : le mot
- * \param d : le dictionnaire
+ * \param dictionnaire : le dictionnaire
+ * \return MOT_TableauDeMots, c'est-à-dire les mots pouvant être une correction de celui pris en entrée.
  */
 
 MOT_TableauDeMots CO_proposerMots(MOT_Mot m, DICTIONNAIRE_Dictionnaire dictionnaire);
 
 
+/**
+ * \fn CO_MotsDansPhrase CO_phraseEnMots(char phrase[])
+ * \brief Fonction qui, à partir d'une phrase, nous renvoie une liste de mots accompagnés de leur position dans la phrase.
+ * \param phrase[] : la phrase que l'on souhaite décomposée en plusieurs mots.
+ * \return CO_MotsDansPhrase, c'est-à-dire les mots d'une phrase mais de façon séparée avec de leur position dans la phrase.
+ */
+
+CO_MotsDansPhrase CO_phraseEnMots(char phrase[]);
 
 #endif
