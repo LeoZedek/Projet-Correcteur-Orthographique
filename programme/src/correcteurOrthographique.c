3885b8b0aaf3c7a35f5487fae02afb4eeaeaff8d
Ruth Lioté <ruth.liote@insa-rouen.fr>
2021-12-22 17:20:31 +0100

    Implémentation de la fonction CO_tableauDEntiersVide dans le .c

--- programme/src/correcteurOrthographique.c
+++ programme/src/correcteurOrthographique.c
@@ -1,3 +1,14 @@
-int correcteurOrthographique(){
-	return 0 ;
+#include <stdlib.h>
+#include "mot.h"
+#include "dictionnaire.h"
+#include "correcteurOrthographique.h"
+
+CO_TableauDEntiers CO_tableauDEntiersVide(){
+	CO_TableauDEntiers tab;
+	tab.lesEntiers = NULL;
+	tab.longeur = 0;
+	return tab;
 }
+
+
+
